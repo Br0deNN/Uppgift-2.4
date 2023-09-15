66f2693b3287a3a03816db4d22e11488f4cfed2d
7R1XY <theo.broden@gmail.com>
2023-09-15 13:12:08 +0200

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad tjänar den första anställda?");
+            string förstaText = Console.ReadLine();
+            double första = double.Parse(förstaText);
+            Console.WriteLine("Vad tjänar den andra anställda?");
+            string andratext = Console.ReadLine();  
+            double andra = double.Parse(andratext);
+            Console.WriteLine("Vad tjänar den tredje anställda?");
+            string tredjetext = Console.ReadLine(); 
+            double tredje = double.Parse(tredjetext);   
+            
+            double sum = (första + andra + tredje) / 2;
+            Console.WriteLine("Medelvärdet av de tre antälldas lön är " +  sum);
+            Console.ReadKey();
+        }
+    }
+}
